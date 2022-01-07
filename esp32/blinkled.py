568ecac4e1723d5aa11b6c288f2f8323ae15c9fa
Fernando González (Fëgôr) <fegor@fegor.com>
2022-01-07 11:18:41 +0100

    MicroPython develop...

---
+++ esp32/blinkled.py
@@ -0,0 +1,10 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+  led.value(not led.value())
+  sleep(0.5)
