87454d43b37322a55e03a2d5099e2e4b599e8921
Fernando González (Fëgôr) <fegor@fegor.com>
2022-03-10 16:17:32 +0100

    Delete directories

--- kk/petstore/__init__.py
+++
@@ -1,4 +0,0 @@
-from swagger_codegen.api.configuration import Configuration
-from .client import new_client
-
-__all__ = ('new_client', 'Configuration')
--- pymq/tail_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from test import support
-
-class TailTestCase1(unittest.TestCase):
-    def test_create(self):
-
-if __name__ == '__main__':
-    unittest.main()
